39c329275efa3973cae01a9938148b6c9fc5de53
luishaaa <luish@DESKTOP-N4VTFOJ>
2020-10-30 22:24:23 -0500

    Agregar archivos de proyecto.

---
+++ SistemaCalificaciones/Login.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SistemaCalificaciones
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+            
+        }
+
+        private async void btnIngreso_Clicked(object sender, EventArgs e)
+        {
+            string us = "estudiante2020";
+            string clave = "uisrael2020";
+            string user = txtUsuario.Text;
+            string pass = txtClave.Text;
+            if (user == us && clave == pass)
+            {
+                await Navigation.PushModalAsync(new MainPage(user, pass));
+            }
+            else
+            {
+                DisplayAlert("Error", "Usuario o Contraseña Incorrecto", "Aceptar");
+            }
+
+        }
+    }
+}
